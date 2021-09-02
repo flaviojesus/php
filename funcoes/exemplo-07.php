df5abcd27f3443bf6653f8080b30a5aaa4a36cec
ASP FLÁVIO SANTOS CCA-BR <00876881150@ccabr.intraer>
2021-09-02 16:08:51 -0300

    Seção 7: atualização de conflitos

--- funcoes/exemplo-07.php
+++ funcoes/exemplo-07.php
@@ -1,8 +1,6 @@
 <?php
 //FUNÇÕES NO PHP 7 NOVIDADES
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 function soma(int ...$valores) {//int ...$valores (todos os parâmetros serão inteiros)
 
     return array_sum($valores); //soma todos os valores do array
@@ -15,13 +13,5 @@ echo soma(25, 33);
 echo "<br>";
 echo soma(1.5, 3.2); //ignora o "." (somou 1 + 3)
 echo "<br>";
-=======
-    
-
->>>>>>> 15e8d9c3cad30e31a8580788ae1e8227431b1545
-=======
-    
-
->>>>>>> 15e8d9c3cad30e31a8580788ae1e8227431b1545
 
 ?>
